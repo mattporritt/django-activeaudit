00fe8157768d04a411791b358013a4d3ac0a4d26
Matt Porritt <mattp@catalyst-au.net>
2021-01-02 04:54:50 +0000

    add license header to existing files

--- activeaudit/urls.py
+++ activeaudit/urls.py
@@ -1,4 +1,29 @@
-"""activeaudit URL Configuration
+# ==============================================================================
+#
+# This file is part of ActiveAudit.
+#
+# ActiveAudit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ActiveAudit is distributed  WITHOUT ANY WARRANTY:
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# ==============================================================================
+
+# ==============================================================================
+#
+# @author Matthew Porritt
+# @copyright  2021 onwards Matthew Porritt (mattp@catalyst-au.net)
+# @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# ==============================================================================
+
+"""
+ActiveAudit URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- manage.py
+++ manage.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+
+# ==============================================================================
+#
+# This file is part of ActiveAudit.
+#
+# ActiveAudit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ActiveAudit is distributed  WITHOUT ANY WARRANTY:
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# ==============================================================================
+
+# ==============================================================================
+#
+# @author Matthew Porritt
+# @copyright  2021 onwards Matthew Porritt (mattp@catalyst-au.net)
+# @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# ==============================================================================
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
