1418d80727f4206ef595e971e5e525f877f634bd
Matt Porritt <mattp@catalyst-au.net>
2021-01-11 00:45:28 +0000

    cusotm asgi handler for websockets

--- activeaudit/asgi.py
+++ activeaudit/asgi.py
@@ -32,9 +32,36 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import django
+from websocket import views
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'activeaudit.settings')
 
-application = get_asgi_application()
+
+async def application(scope, receive, send):
+    """
+    Custom application definition for asynchronous handling of websockets.
+    The Django async implementation only handles HTTP requests, so we
+    need to do some custom work.
+    """
+
+    # If we receive an HTTP request instead of a websocket request,
+    # tell the caller they need to upgrade.
+    if scope["type"] == "http":
+        await send({
+            'type': 'http.response.start',
+            'status': 426,
+            'headers': [
+                (b'content-type', b'text/plain'),
+                (b'content-length', b'84'),
+            ],
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': b'This service requires use of the Websocket protocol',
+        })
+    # For websocket connections: first call Django setup then hand off
+    # to the websocket application.
+    elif scope['type'] == 'websocket':
+        django.setup(set_prefix=False)
+        await views.websocket_application(scope, receive, send)
