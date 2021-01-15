895741219d8161777e19302202c993377d82328d
Matt Porritt <mattp@catalyst-au.net>
2021-01-15 05:15:04 +0000

    initial testrig view

--- testrig/views.py
+++ testrig/views.py
@@ -1,3 +1,33 @@
+# ==============================================================================
+#
+# This file is part of ActiveAudit.
+#
+# ActiveAudit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ActiveAudit is distributed  WITHOUT ANY WARRANTY:
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# ==============================================================================
+
+# ==============================================================================
+#
+# @author Matthew Porritt
+# @copyright  2021 onwards Matthew Porritt (mattp@catalyst-au.net)
+# @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+# ==============================================================================
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context = {}
+
+    # Pass the context to a template
+    return render(request, 'testrig/index.html', context)
+
