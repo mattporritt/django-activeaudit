9730b1f115a1f535fd95806e5b6cb07584e1326d
Matt Porritt <mattp@catalyst-au.net>
2021-01-18 01:31:34 +0000

    working grunt implementation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,28 @@
-module.exports = function(grunt) {
-  // Do grunt-related things in here
+module.exports = grunt => {
+    const sass = require('node-sass');
+
+    // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+    require('load-grunt-tasks')(grunt)
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'activeaudit/staticfiles/assets/css/main.css': 'activeaudit/staticfiles/scss/main.scss'
+                }
+            }
+        },
+        shell: {
+            collectStatic: {
+                command: './manage.py collectstatic --noinput'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass', 'shell']);
 };
