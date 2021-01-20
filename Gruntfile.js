768984c344da0cde0fdfb8215d189f3f9198a10d
Matt Porritt <mattp@catalyst-au.net>
2021-01-20 02:41:26 +0000

    add brotli compression to static files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = grunt => {
     const sass = require('node-sass');
+    const zlib = require('zlib');
 
     // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
     require('load-grunt-tasks')(grunt)
@@ -41,8 +42,32 @@ module.exports = grunt => {
             collectStatic: {
                 command: './manage.py collectstatic --noinput'
             }
+        },
+        compress: {
+            main: {
+                options: {
+                    mode: 'brotli',
+                    brotli: {
+                        mode: 0,
+                        quality: 11
+                    },
+                    pretty: true
+                },
+                files: [
+                    {expand: true, cwd: 'static/', src: ['**/*.js'], dest: 'static/', extDot: 'last', ext: '.js.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.css'], dest: 'static/', extDot: 'last', ext: '.css.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.map'], dest: 'static/', extDot: 'last', ext: '.map.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.eot'], dest: 'static/', extDot: 'last', ext: '.eot.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.svg'], dest: 'static/', extDot: 'last', ext: '.svg.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.woff'], dest: 'static/', extDot: 'last', ext: '.woff.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.woff2'], dest: 'static/', extDot: 'last', ext: '.woff2.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.png'], dest: 'static/', extDot: 'last', ext: '.png.br'},
+                    {expand: true, cwd: 'static/', src: ['**/*.jpg'], dest: 'static/', extDot: 'last', ext: '.jpg.br'},
+                ]
+
+            }
         }
     });
 
-    grunt.registerTask('default', ['sass', 'cssmin', 'shell']);
+    grunt.registerTask('default', ['sass', 'cssmin', 'shell', 'compress']);
 };
